f9274633b6069b2bd0a11a84745c186b93a1724c
Hui Wang <huiw3939@gmail.com>
2017-07-18 01:05:08 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,13 @@
+require 'pry'
+
 def count_elements(array)
-  # code goes here
+  hash = {}
+  array.each do |x|
+    if hash.include?(x) == false
+      hash[x] = 1
+    else
+      hash[x]+=1
+    end
+  end
+  hash
 end
- 
